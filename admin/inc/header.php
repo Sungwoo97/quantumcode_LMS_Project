4cef9453ab5132167655d328e825e370bf77bcf2
bluedawn123 <haemilyjh@naver.com>
2024-11-07 03:31:03 +0900

    테스트용 db 불러오기

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,5 +1,12 @@
+<?php
+/*
+여기서 기존과 다르게 바로 admin/inc/header.php로 들어가는점은, 이전htdocs/abcmall/admin/inc/header.php 
+경우에는 DOCUMENT_ROOT가 htdocs이지만 우리의 경우, DOCUMENT_ROOT가 3nd_projecct 이후 별도의 폴더 없이
+바로 프로젝트를 실행해서 이렇게 실행됩니다. 
+*/
 
-
+include_once($_SERVER['DOCUMENT_ROOT'].'/admin/inc/dbcon.php');
+?>
 
 <!DOCTYPE html>
 <html lang="en">
