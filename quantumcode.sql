2a78026a40a8445970936af0c3463a1cbc3b12bc
bluedawn123 <haemilyjh@naver.com>
2024-11-07 16:25:31 +0900

    디비 추가

---
+++ quantumcode.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- 생성 시간: 24-11-07 08:24
+-- 서버 버전: 10.4.32-MariaDB
+-- PHP 버전: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `quantumcode`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `admins`
+--
+
+CREATE TABLE `admins` (
+  `idx` int(11) NOT NULL,
+  `userid` varchar(145) DEFAULT NULL,
+  `email` varchar(245) DEFAULT NULL,
+  `username` varchar(145) DEFAULT NULL,
+  `passwd` varchar(200) DEFAULT NULL,
+  `regdate` datetime DEFAULT current_timestamp(),
+  `level` tinyint(4) DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `end_login_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 덤프된 테이블의 인덱스
+--
+
+--
+-- 테이블의 인덱스 `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`idx`);
+
+--
+-- 덤프된 테이블의 AUTO_INCREMENT
+--
+
+--
+-- 테이블의 AUTO_INCREMENT `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
