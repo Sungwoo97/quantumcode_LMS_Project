ea3678d9e19ac0047db1f59da14609be70477b10
Sungwoo97 <gwaja97@gmail.com>
2024-11-07 18:25:28 +0900

    lecture table 임시 저장

--- quantumcode.sql
+++ quantumcode.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 24-11-07 09:26
+-- 생성 시간: 24-11-07 10:24
 -- 서버 버전: 10.4.32-MariaDB
 -- PHP 버전: 8.2.12
 
@@ -44,7 +44,64 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`idx`, `userid`, `email`, `username`, `passwd`, `regdate`, `level`, `last_login`, `end_login_date`) VALUES
-(4, 'admin', 'admin@shop.com', '관리자', '33275a8aa48ea918bd53a9181aa975f15ab0d0645398f5918a006d08675c1cb27d5c645dbd084eee56e675e25ba4019f2ecea37ca9e2995b49fcb12c096a032e', '2023-01-01 17:12:32', 100, '2024-11-07 17:23:35', NULL);
+(4, 'admin', 'admin@shop.com', '관리자', '33275a8aa48ea918bd53a9181aa975f15ab0d0645398f5918a006d08675c1cb27d5c645dbd084eee56e675e25ba4019f2ecea37ca9e2995b49fcb12c096a032e', '2023-01-01 17:12:32', 100, '2024-11-07 17:28:44', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `lecture_category`
+--
+
+CREATE TABLE `lecture_category` (
+  `lcid` int(11) NOT NULL COMMENT '카테고리 고유번호',
+  `code` varchar(20) NOT NULL COMMENT '카테고리코드',
+  `pcode` varchar(20) DEFAULT NULL COMMENT '카테고리 부모코드',
+  `name` varchar(100) NOT NULL COMMENT '카테고리 이름',
+  `step` tinyint(4) NOT NULL COMMENT '카테고리 분류 단계'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='강의 카테고리';
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `lecture_list`
+--
+
+CREATE TABLE `lecture_list` (
+  `lid` int(11) NOT NULL COMMENT '강의 고유번호',
+  `category` varchar(100) NOT NULL COMMENT '강의 카테고리',
+  `title` varchar(500) NOT NULL COMMENT '강의 제목',
+  `cover_image` varchar(100) DEFAULT NULL COMMENT '강의 커버 이미지',
+  `tid` varchar(100) NOT NULL COMMENT '강사이름',
+  `isfree` tinyint(4) NOT NULL COMMENT '무료강의',
+  `ispremium` tinyint(4) NOT NULL COMMENT '프리미엄강의',
+  `ispopular` tinyint(4) NOT NULL COMMENT '인기강의',
+  `isrecom` tinyint(4) NOT NULL COMMENT '추천강의',
+  `tuition` double NOT NULL COMMENT '수강료',
+  `dis_tuition` double DEFAULT NULL COMMENT '할인 수강료',
+  `regist_day` datetime NOT NULL COMMENT '수강시작일',
+  `expiration_day` datetime DEFAULT NULL COMMENT '수강마감일',
+  `sub_title` varchar(250) DEFAULT NULL COMMENT '강의 요약',
+  `description` text NOT NULL COMMENT '강의 설명',
+  `learning_obj` text DEFAULT NULL COMMENT '강의 목표',
+  `difficult` varchar(11) NOT NULL COMMENT '난이도',
+  `pr_video` varchar(100) DEFAULT NULL COMMENT '홍보 영상',
+  `regdate` datetime NOT NULL DEFAULT current_timestamp() COMMENT '작성시간',
+  `status` tinyint(4) NOT NULL COMMENT '상태'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='강의 목록 테이블';
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `lecture_video`
+--
+
+CREATE TABLE `lecture_video` (
+  `lvid` int(11) NOT NULL COMMENT '강의영상 고유번호',
+  `lid` int(11) NOT NULL COMMENT '연결된 강의 고유번호',
+  `video_lecture` varchar(100) NOT NULL COMMENT '강의 영상 파일경로',
+  `video_desc` text DEFAULT NULL COMMENT '강의 설명',
+  `regdate` datetime NOT NULL DEFAULT current_timestamp() COMMENT '등록 시간'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='강의 영상 테이블';
 
 --
 -- 덤프된 테이블의 인덱스
@@ -57,6 +114,24 @@ ALTER TABLE `admins`
   ADD PRIMARY KEY (`idx`);
 
 --
+-- 테이블의 인덱스 `lecture_category`
+--
+ALTER TABLE `lecture_category`
+  ADD PRIMARY KEY (`lcid`);
+
+--
+-- 테이블의 인덱스 `lecture_list`
+--
+ALTER TABLE `lecture_list`
+  ADD PRIMARY KEY (`lid`);
+
+--
+-- 테이블의 인덱스 `lecture_video`
+--
+ALTER TABLE `lecture_video`
+  ADD PRIMARY KEY (`lvid`);
+
+--
 -- 덤프된 테이블의 AUTO_INCREMENT
 --
 
@@ -65,6 +140,24 @@ ALTER TABLE `admins`
 --
 ALTER TABLE `admins`
   MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- 테이블의 AUTO_INCREMENT `lecture_category`
+--
+ALTER TABLE `lecture_category`
+  MODIFY `lcid` int(11) NOT NULL AUTO_INCREMENT COMMENT '카테고리 고유번호';
+
+--
+-- 테이블의 AUTO_INCREMENT `lecture_list`
+--
+ALTER TABLE `lecture_list`
+  MODIFY `lid` int(11) NOT NULL AUTO_INCREMENT COMMENT '강의 고유번호';
+
+--
+-- 테이블의 AUTO_INCREMENT `lecture_video`
+--
+ALTER TABLE `lecture_video`
+  MODIFY `lvid` int(11) NOT NULL AUTO_INCREMENT COMMENT '강의영상 고유번호';
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
