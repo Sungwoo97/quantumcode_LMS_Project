84ac7d0aec743ac41fee6d41560a4dce1b6ad773
bluedawn123 <haemilyjh@naver.com>
2024-11-07 17:27:05 +0900

    admin 인스턴스 생성

--- quantumcode.sql
+++ quantumcode.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 24-11-07 08:24
+-- 생성 시간: 24-11-07 09:26
 -- 서버 버전: 10.4.32-MariaDB
 -- PHP 버전: 8.2.12
 
@@ -40,6 +40,13 @@ CREATE TABLE `admins` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- 테이블의 덤프 데이터 `admins`
+--
+
+INSERT INTO `admins` (`idx`, `userid`, `email`, `username`, `passwd`, `regdate`, `level`, `last_login`, `end_login_date`) VALUES
+(4, 'admin', 'admin@shop.com', '관리자', '33275a8aa48ea918bd53a9181aa975f15ab0d0645398f5918a006d08675c1cb27d5c645dbd084eee56e675e25ba4019f2ecea37ca9e2995b49fcb12c096a032e', '2023-01-01 17:12:32', 100, '2024-11-07 17:23:35', NULL);
+
+--
 -- 덤프된 테이블의 인덱스
 --
 
@@ -57,7 +64,7 @@ ALTER TABLE `admins`
 -- 테이블의 AUTO_INCREMENT `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
