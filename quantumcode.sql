d6c57e36e4f6a9bc7dd31576559aa2ded7c22939
yujin-1092 <kunche1092@gmail.com>
2024-11-08 16:19:06 +0900

    coupon_테이블 sql 업로드

--- quantumcode.sql
+++ quantumcode.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 24-11-07 09:56
+-- 생성 시간: 24-11-08 08:17
 -- 서버 버전: 10.4.32-MariaDB
 -- PHP 버전: 8.2.12
 
@@ -44,67 +44,42 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`idx`, `userid`, `email`, `username`, `passwd`, `regdate`, `level`, `last_login`, `end_login_date`) VALUES
-(4, 'admin', 'admin@shop.com', '관리자', '33275a8aa48ea918bd53a9181aa975f15ab0d0645398f5918a006d08675c1cb27d5c645dbd084eee56e675e25ba4019f2ecea37ca9e2995b49fcb12c096a032e', '2023-01-01 17:12:32', 100, '2024-11-07 17:43:01', NULL);
+(4, 'admin', 'admin@shop.com', '관리자', '33275a8aa48ea918bd53a9181aa975f15ab0d0645398f5918a006d08675c1cb27d5c645dbd084eee56e675e25ba4019f2ecea37ca9e2995b49fcb12c096a032e', '2023-01-01 17:12:32', 100, '2024-11-07 17:42:16', NULL);
 
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `event_board`
+-- 테이블 구조 `coupons_list`
 --
 
-CREATE TABLE `event_board` (
-  `pid` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `event_date` date DEFAULT NULL,
-  `user_id` varchar(10) DEFAULT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+CREATE TABLE `coupons_list` (
+  `cid` int(11) NOT NULL,
+  `coupon_name` varchar(100) NOT NULL COMMENT '쿠폰명',
+  `coupon_image` varchar(100) NOT NULL COMMENT '쿠폰이미지',
+  `coupon_type` varchar(100) NOT NULL COMMENT '쿠폰타입',
+  `coupon_price` double DEFAULT NULL COMMENT '할인금액',
+  `coupon_ratio` double DEFAULT NULL COMMENT '할인비율',
+  `status` tinyint(4) DEFAULT 0 COMMENT '상태',
+  `regdate` datetime DEFAULT current_timestamp() COMMENT '등록일',
+  `userid` varchar(100) DEFAULT NULL COMMENT '등록한유저',
+  `max_value` double DEFAULT NULL COMMENT '최대할인금액',
+  `use_min_price` double DEFAULT NULL COMMENT '최소사용금액'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- 테이블 구조 `free_board`
+-- 테이블 구조 `coupons_usercp`
 --
 
-CREATE TABLE `free_board` (
-  `pid` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `user_id` varchar(10) DEFAULT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- 테이블 구조 `notice_board`
---
-
-CREATE TABLE `notice_board` (
-  `pid` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `user_id` varchar(10) DEFAULT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- 테이블 구조 `qna_board`
---
-
-CREATE TABLE `qna_board` (
-  `pid` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `user_id` varchar(10) DEFAULT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+CREATE TABLE `coupons_usercp` (
+  `ucid` int(11) NOT NULL,
+  `couponid` int(11) NOT NULL COMMENT '쿠폰아이디',
+  `userid` varchar(100) NOT NULL COMMENT '유저아이디',
+  `status` int(11) DEFAULT 1 COMMENT '상태',
+  `use_max_date` datetime DEFAULT NULL COMMENT '만료일',
+  `regdate` datetime DEFAULT current_timestamp() COMMENT '발급일',
+  `reason` varchar(100) NOT NULL COMMENT '쿠폰사용강의'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -118,28 +93,16 @@ ALTER TABLE `admins`
   ADD PRIMARY KEY (`idx`);
 
 --
--- 테이블의 인덱스 `event_board`
+-- 테이블의 인덱스 `coupons_list`
 --
-ALTER TABLE `event_board`
-  ADD PRIMARY KEY (`pid`);
+ALTER TABLE `coupons_list`
+  ADD PRIMARY KEY (`cid`);
 
 --
--- 테이블의 인덱스 `free_board`
+-- 테이블의 인덱스 `coupons_usercp`
 --
-ALTER TABLE `free_board`
-  ADD PRIMARY KEY (`pid`);
-
---
--- 테이블의 인덱스 `notice_board`
---
-ALTER TABLE `notice_board`
-  ADD PRIMARY KEY (`pid`);
-
---
--- 테이블의 인덱스 `qna_board`
---
-ALTER TABLE `qna_board`
-  ADD PRIMARY KEY (`pid`);
+ALTER TABLE `coupons_usercp`
+  ADD PRIMARY KEY (`ucid`);
 
 --
 -- 덤프된 테이블의 AUTO_INCREMENT
@@ -152,28 +115,16 @@ ALTER TABLE `admins`
   MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- 테이블의 AUTO_INCREMENT `event_board`
+-- 테이블의 AUTO_INCREMENT `coupons_list`
 --
-ALTER TABLE `event_board`
-  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `coupons_list`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- 테이블의 AUTO_INCREMENT `free_board`
+-- 테이블의 AUTO_INCREMENT `coupons_usercp`
 --
-ALTER TABLE `free_board`
-  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- 테이블의 AUTO_INCREMENT `notice_board`
---
-ALTER TABLE `notice_board`
-  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- 테이블의 AUTO_INCREMENT `qna_board`
---
-ALTER TABLE `qna_board`
-  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `coupons_usercp`
+  MODIFY `ucid` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
