5b1be184646b79b3193e45a75a7994a79651252d
Sungwoo97 <gwaja97@gmail.com>
2024-11-10 23:56:57 +0900

    common.css 추가

--- admin/lecture/lecture_insert_ok.php
+++ admin/lecture/lecture_insert_ok.php
@@ -1,5 +1,6 @@
 <?php
   include_once($_SERVER['DOCUMENT_ROOT'] . '/admin/inc/dbcon.php');
+  include_once($_SERVER['DOCUMENT_ROOT'] . '/admin/inc/common.php');
 
   $mysqli->autocommit(FALSE);//커밋이 안되도록 지정, 일단 바로 저장하지 못하도록
 
@@ -11,35 +12,61 @@
     $lecture_technologies = $_POST['technologies'] ?? '';
     $lecture_tuition = $_POST['tuition'] ?? 0;
     $lecture_disTuition = $_POST['dis_tuition'] ?? 0;
-    $lecture_regist_day = $_POST['regist_day'] ?? 0;
+    $lecture_registDay = $_POST['regist_day'] ?? 0;
     $lecture_difficult = $_POST['difficult'] ?? '';
     $lecture_ispremium = $_POST['ispremium'] ?? 0;
     $lecture_ispopular = $_POST['ispopular'] ?? 0;
     $lecture_isrecom = $_POST['isrecom'] ?? 0;
     $lecture_isfree = $_POST['isfree'] ?? 0;
     $lecture_subTitle = $_POST['sub_title'] ?? '';
-    $lecture_desc = $_POST['desc'];
+    $lecture_desc = rawurldecode($_POST['lecture_description']);
+    
     $lucture_objectives = $_POST['objectives'] ?? '';
     $lecture_tag = $_POST['tag'] ?? '';
 
-    $lecture_coverImage = $_FILES['cover_image'] ?? '';
-    $lecture_prVideo = $_FILES['pr_video'] ?? '';
+    $lecture_coverImage = $_FILES['cover_image'] ?? null;
+    $lecture_prVideo = null;
     $lecture_prVideoUrl = $_POST['pr_videoUrl'] ?? '';
     // $lecture_addVideosUrl = $_FILES['add_videosUrl'];
 
-    $expiration_day = strtotime("{$lecture_regist_day}-1 months");
+
+    $expiration_day = date("Y-m-d", strtotime("+3 months", strtotime($lecture_registDay)));
 
     $lecture_cate = $lecture_platforms.$lecture_development.$lecture_technologies;
-  
+    
+    if (isset($_FILES['cover_image']) && $_FILES['cover_image']['error'] == UPLOAD_ERR_OK)  {
+      $fileUploadResult = fileUpload($_FILES['cover_image'], 'image' );
+      if($fileUploadResult) {
+          $lecture_coverImage = $fileUploadResult;
+      } else {
+          echo "<script>
+              alert('파일 첨부할 수 없습니다.');
+              history.back();
+          </script>";
+      }
+    }
+
+    if (isset($_FILES['pr_video']) && $_FILES['pr_video']['error'] == UPLOAD_ERR_OK)  {
+      $fileUploadResult = fileUpload($_FILES['pr_video'], 'video' );
+      if($fileUploadResult) {
+          $lecture_prVideo = $fileUploadResult;
+      } else {
+          echo "<script>
+              alert('파일 첨부할 수 없습니다.');
+              history.back();
+          </script>";
+      }
+    }  
     
 
-    $lecture_Sql = "INSERT INTO lecture_list 
-    (category, title, cover_image, isfree, ispremium, ispopular, isrecom, tuition, dis_tuition, regist_day, expiration_day, sub_title, description, learning_obj, difficult, lecture tag, pr_video )
+    $lecture_sql = "INSERT INTO lecture_list 
+    (category, title, cover_image, isfree, ispremium, ispopular, isrecom, tuition, dis_tuition, regist_day, expiration_day, sub_title, description, learning_obj, difficult, lecture_tag, pr_video )
     VALUES
-    ()
+    ('$lecture_cate', '$lecture_title', '$lecture_coverImage', $lecture_isfree, $lecture_ispremium, $lecture_ispopular, $lecture_isrecom, $lecture_tuition, $lecture_disTuition, $lecture_registDay, $expiration_day, '$lecture_subTitle', '$lecture_desc', '$lucture_objectives', $lecture_difficult, '$lecture_tag', '$lecture_prVideo');
     ";
 
-    echo $lecture_Sql;
+    $lecture_result = $mysqli->query($lecture_sql);
+
 
   }catch (Exception $e) {
     $mysqli->rollback();//저장한 테이블이 있다면 롤백한다.
