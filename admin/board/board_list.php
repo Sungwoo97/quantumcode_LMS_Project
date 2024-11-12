a04fa6f26a7188b397b263810f95c20e93eca6bf
Naseungil <sktmddlf505@naver.com>
2024-11-12 12:56:31 +0900

    보드 리스트 작업중

--- admin/board/board_list.php
+++ admin/board/board_list.php
@@ -2,32 +2,44 @@
 include_once($_SERVER['DOCUMENT_ROOT'].'/admin/inc/header.php');
 ?>
 
-<?php
+<div class="container">
+  <h2>전체 게시판</h2>
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th scope="col">No</th>
+        <th scope="col">제목</th>
+        <th scope="col">내용</th>
+        <th scope="col">작성일</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      // 데이터베이스에서 게시글 목록 조회
+      $sql = "SELECT * FROM board ORDER BY pid DESC";
+      $result = mysqli_query($sql);
 
-// 전체 게시판 조회
-$sql = "SELECT * FROM posts ORDER BY created_at DESC";
-$result = mysqli_query($sql);
-
-$list = '';
-while($data = $result -> fetch_object()){
-  $title = $data->title;
-  
-
-  if(iconv_strlen($title)>10){
-    $title = iconv_substr($title, 0, 10) . '...';
-  }
-?>
-  <tr>
-    <th scope="row"><?=$data->pid ?></th>
-    <td><?=$title ?></td>
-    <td><?=$data->content ?></td>
-    <td><?=$data->created_at ?></td>
-  </tr>
-<?php
-}
-?>
-<?=$list?>
-?>
+      // 게시글 출력
+      while($data = $result->fetch_object()){
+        $title = $data->title;
+        
+        // 제목이 길 경우 10글자로 자르기
+        if(iconv_strlen($title) > 10){
+          $title = iconv_substr($title, 0, 10) . '...';
+        }
+      ?>
+      <tr>
+        <th scope="row"><?=$data->pid ?></th>
+        <td><?=$title ?></td>
+        <td><?=$data->content ?></td>
+        <td><?=$data->date ?></td>
+      </tr>
+      <?php
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
 
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/admin/inc/footer.php');
