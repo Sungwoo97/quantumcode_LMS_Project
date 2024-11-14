1fcf4fac4ac01fa1a8c1b10e094e5672eaaae3e7
bluedawn123 <haemilyjh@naver.com>
2024-11-14 18:24:46 +0900

    강사 정보 디비에 등록하기(거의다함)

--- admin/teachers/teacher_insert_ok.php
+++ admin/teachers/teacher_insert_ok.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-include_once($_SERVER['DOCUMENT_ROOT'] . '/admin/inc/dbcon.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/qc/admin/inc/dbcon.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/qc/admin/inc/common.php');
 
 // if(!isset($_SESSION['AUID'])){
 //   echo"
@@ -11,18 +12,48 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/admin/inc/dbcon.php');
 //   ";
 // }
 
-$name = $_FILES['name'] ?? '';
-$id = $_FILES['id'] ?? '';
-$birth = $_FILES['birth'] ?? '';
-$password = $_FILES['password'] ?? '';
+$name = $_POST['name'] ?? '';
+$id = $_POST['id'] ?? '';
+$birth = $_POST['birth'] ?? '';
+$password = $_POST['password'] ?? '';
 // $passwordCheck = $_FILES['passwordCheck'] ?? '';
-$email = $_FILES['email'] ?? '';
-$number = $_FILES['number'] ?? '';
-$reg_date = $_FILES['reg_date'] ?? '';
-$teacher_detail = $_FILES['teacher_detail'] ?? '';
-
-
-$grade = $_FILES['grade'] ?? '';
+$email = $_POST['email'] ?? '';
+$number = $_POST['number'] ?? '';
+$reg_date = $_POST['reg_date'] ?? '';
+$teacher_detail =$_POST['teacher_detail'] ?? '';
+$grade = $_POST['grade'] ?? '';
 $cover_image = $_FILES['cover_image'] ?? '';
+print_r($cover_image);
 
+
+if (isset($_FILES['cover_image']) && $_FILES['cover_image']['error'] == UPLOAD_ERR_OK) {
+  $fileUploadResult = fileUpload($_FILES['cover_image'], 'image');
+  if ($fileUploadResult) {
+    $teacher_coverImage = $fileUploadResult;
+  } else {
+    echo "<script>
+              alert('파일 첨부할 수 없습니다.');
+              history.back();
+          </script>";
+  }
+}
+
+
+$sql = "INSERT INTO teachers
+(name, id, birth, password, email, number, reg_date, teacher_detail, grade, cover_image)
+VALUES
+($name, $id, $birth, $password, $email, $number, $reg_date, $teacher_detail, $grade, $cover_image)";
+
+echo $sql;
+
+$teacher_result = $mysqli->query($sql);
+if ($teacher_data = $teacher_result->fetch_object()) {
+  "<script>
+    alert('강사가 등록되었습니다.');
+    </script>";
+}
+
+
+
+$mysqli->close() ;
 ?>
