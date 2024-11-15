7e2f249db52a287518b57f2eabca4acea4622ade
bluedawn123 <haemilyjh@naver.com>
2024-11-15 16:45:26 +0900

    로그인, 로그아웃 세션 구현

--- admin/login_ok_teacher.php
+++ admin/login_ok_teacher.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+// session_start(); 이거 살려야함? 지워야함?? header.php에 있으니깐 중복이라고 자꾸 나와서 일단 지워봄
 
 include_once($_SERVER['DOCUMENT_ROOT'].'/qc/admin/inc/header.php');
 
@@ -16,7 +16,7 @@ if($data){
   $_SESSION['AUNAME'] = $data->name;
 
   echo "<script>
-    alert('강사님 반갑습니다.');
+    alert('$data->name 님 반갑습니다. 아이디는 $data->id 입니다.');
     location.href='index.php';
   </script>";
 
