fbda8a9f867cf9b8ddb382d4545948c1eb00ef27
Naseungil <sktmddlf505@naver.com>
2024-11-17 22:59:14 +0900

    이미지 기능 추가

--- admin/board/board_write_ok.php
+++ admin/board/board_write_ok.php
@@ -1,42 +1,60 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/qc/admin/inc/dbcon.php');
 
-// board_write_ok.php 예시
 $category = $_POST['category'];
 $title1 = $_POST['title'];
 $content = $_POST['content'];
+//print_r($_FILES['file']['name']);
+
+//파일 업로드
+$file_name = $_FILES['file']['name'];
+$temp_path = $_FILES['file']['tmp_name'];
+$upload_path = './upload/'.$file_name;
+move_uploaded_file($temp_path,$upload_path);
+
+strpos($_FILES['file']['type'], 'image') !== false ? $is_img = 1 : $is_img = 0;
 
 
 
-$sql = "INSERT INTO board (category, title, content) VALUES ('$category', '$title1', '$content')";
+$max_file_size = 10*1024*1024;
+
+if($_FILES['file']['size'] >$max_file_size ){
+  echo "<script>
+      alert('10MB 이상은 첨부할수 없습니다.');
+      history.back();
+  </script>";
+}
+
+
+$sql = "INSERT INTO board (category, title, content, img, is_img) VALUES ('$category', '$title1', '$content', '$upload_path', $is_img)";
 $result = $mysqli->query($sql);
 
 switch ($category) {
-    case 'qna':
-        $redirect_url = '/qc/admin/board/board_list.php?category=qna';
-        break;
-    case 'notice':
-        $redirect_url = '/qc/admin/board/board_list.php?category=notice';
-        break;
-    case 'event':
-        $redirect_url = '/qc/admin/board/board_list.php?category=event';
-        break;
-    case 'free':
-        $redirect_url = '/qc/admin/board/board_list.php?category=free';
-        break;
-    default:
-        die("카테고리를 선택 해주세요.");
+  case 'qna':
+      $redirect_url = '/qc/admin/board/board_list.php?category=qna';
+      break;
+  case 'notice':
+      $redirect_url = '/qc/admin/board/board_list.php?category=notice';
+      break;
+  case 'event':
+      $redirect_url = '/qc/admin/board/board_list.php?category=event';
+      break;
+  case 'free':
+      $redirect_url = '/qc/admin/board/board_list.php?category=free';
+      break;
+  default:
+      die("카테고리를 선택 해주세요.");
 }
 
 if ($result) {
-    echo "<script>
-        alert('글 작성 완료');
-        location.href='$redirect_url';
-    </script>";
+  echo "<script>
+      alert('글 작성 완료');
+      location.href='$redirect_url';
+  </script>";
 } else {
-    echo "<script>
-        alert('글 작성 실패');
-        history.back();
-    </script>";
+  echo "<script>
+      alert('글 작성 실패');
+      history.back();
+  </script>";
 }
 ?>
