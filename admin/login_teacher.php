78bdeb7ef93464bdfbd7e1949c4b9b90f1c2f03c
yujin-1092 <kunche1092@gmail.com>
2024-11-18 06:14:56 +0900

    강사/어드민 로그인 나누기

--- admin/login_teacher.php
+++ admin/login_teacher.php
@@ -9,7 +9,7 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/qc/admin/inc/dbcon.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Title -->
-    <title>관리자 로그인 - quantumcode</title>
+    <title>강사 로그인 - quantumcode</title>
 
     <!-- Favicon -->
 
@@ -22,7 +22,7 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/qc/admin/inc/dbcon.php');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
   </head>
-<body>
+<body class="teacher_login_php">
 
   <div class="d-flex">
     <aside>
@@ -33,20 +33,30 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/qc/admin/inc/dbcon.php');
     </aside>
 
     <div class="login_container d-flex flex-column justify-content-center align-items-center">
-      <h1 class="main_tt">Admin Account</h1>
+      <h1 class="main_tt text-center mb-4">Log in to your<br>Account</h1>
+      <div class="d-flex justify-content-between login_sns_teacher">
+        <button class="login_google">Google</button>
+        <button class="login_kakaotalk">KaKaoTalk</button>
+      </div>
+      <div class="divider">
+        <span>Or Continue With Email</span>
+      </div>
       <div class="row login_box">
-        <form action="login_ok.php" method="POST">
+        <form action="login_teacher_ok.php" method="POST">
           <div class="form-floating">
             <input type="text" class="form-control" id="userid" name="userid" placeholder="Admin">
-            <label for="userid">Admin</label>
+            <label for="userid">Your Email</label>
           </div>
           <div class="form-floating">
             <input type="password" class="form-control" id="userpw" name="userpw" placeholder="1111">
-            <label for="userpw">1111</label>
+            <label for="userpw">Password</label>
           </div>
           <div class="d-flex justify-content-between">
-            <p class="mt-4 mb-4"><a href="#" class="forgotpw">Forgot Password?</a></p>
-            <p class="mt-4 mb-4"><a href="login_teacher.php" class="login_change">Log in to Teacher</a></p>
+            <div>
+              <p class="mb-0"><a href="#" class="forgotpw">Join Us</a></p>
+              <p><a href="#" class="forgotpw">Forgot Password?</a></p>
+            </div>
+            <p class="mt-4 mb-4"><a href="login.php" class="login_change">Log in to Admin</a></p>
           </div>
           <button class="btn btn-primary">Log in</button>
         </form>
