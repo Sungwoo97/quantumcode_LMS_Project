eaddd07fb64bd6b690e8231f8c373772b5a2adc6
Naseungil <sktmddlf505@naver.com>
2024-11-18 16:06:04 +0900

    인풋 넓이 수정

--- admin/board/board_write.php
+++ admin/board/board_write.php
@@ -13,20 +13,20 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/qc/admin/inc/header.php');
     <input class="form-control" accept="image/*" name="file" type="file" id="file" onchange="previewImage(event)">
   </div>
   <div class="col-8">
-    <select class="form-select w-25" name="category" aria-label="Default select example" required >
+    <select class="form-select w-25 mb-3" name="category" aria-label="Default select example" required >
       <option value="" selected>카테고리 선택</option>
       <option value="notice">공지사항</option>
       <option value="free">자유게시판</option>
       <option value="event">이벤트</option>
       <option value="qna">질문과답변</option>
     </select>
-    <div class="mb-3">
+    <div class="mb-3 d-flex gap-3">
       <label for="title" class="form-label">제목:</label>
-      <input type="text" class="form-control" name="title" id="title" placeholder="제목입력" required>
+      <input type="text" class="form-control w-75" name="title" id="title" placeholder="제목입력" required>
     </div>
-    <div class="mb-3">
+    <div class="mb-3 d-flex gap-3">
       <label for="content" class="form-label">내용:</label>
-      <textarea class="form-control" id="content" name="content" rows="3" value="" required></textarea>
+      <textarea class="form-control w-75" id="content" name="content" rows="3" value="" required></textarea>
     </div>
     <div class="d-flex justify-content-end gap-3">
       <button type="submit" class="btn btn-primary">등록</button>
