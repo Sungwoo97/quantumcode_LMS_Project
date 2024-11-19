05fbb2d3e1c562ca2c1cb2def7249298daf2076c
Sungwoo97 <gwaja97@gmail.com>
2024-11-19 10:21:54 +0900

    매출 관리 디자인

--- admin/sales/sales_management.php
+++ admin/sales/sales_management.php
@@ -3,32 +3,87 @@ $title = '매출 관리';
 $sales_css = "<link href=\"http://{$_SERVER['HTTP_HOST']}/qc/admin/css/sales.css\" rel=\"stylesheet\">";
 include_once($_SERVER['DOCUMENT_ROOT'] . '/qc/admin/inc/header.php');
 ?>
-<pre>
-  매출 관리 테이블
-  총 강의 수
-  총 수강생 
-  평점 
-  총 매출 
-  
-  매월 매출 테이블 
-  달 마다 수익
-  
-  매월 강의 테이블 
-  상위 4개의 강의 수료율 ? 매출 ?
-  
-  강의 정보 테이블
-  강의 수료율 ( 영상 시청 시간과 시청 시간을 비교? )
-  강의명
-  영상시간
-  영상 갯수
-  영상 시청 기간
-  평균 시청 시간 
-  
-  강사 테이블은 따로 ? 
-  혹은 기존 테이블에 추가 ?
-  => 현재 강사 총괄은 어드민 화면인데
-  
-</pre>
+
+<div class="container sales my-4">
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+      <div class="sales_box">
+        <dl>
+          <dt>강의 수</dt>
+          <dd>23 개</dd>
+        </dl>
+      </div>
+    </div>
+    <div class="col">
+      <div class="sales_box">
+        <dl>
+          <dt>총 수강생</dt>
+          <dd>200 명</dd>
+        </dl>
+      </div>
+    </div>
+    <div class="col">
+      <div class="sales_box">
+        <dl>
+          <dt>평점</dt>
+          <dd>4.5 점</dd>
+        </dl>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-12 sales_box">
+      <dl>
+        <dt>총 매출</dt>
+        <dd>10,580,000 원</dd>
+      </dl>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-6 sales_chart">
+      <dl>
+        <dt>이번 달 수익</dt>
+        <dd>2,020,000 원</dd>
+        <dd>991,000원 (96%) </dd>
+      </dl>
+    </div>
+    <div class="col-6 sales_chart">
+      <dl>
+        <dt>종합 매출</dt>
+        <dd>종합 바 차트</dd>
+      </dl>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-6 sales_chart">
+      <dl>
+        <dt>강의 완강률</dt>
+        <dd>도넛 그래프</dd>
+      </dl>
+    </div>
+    <div class="col-6 sales_chart">
+      <dl>
+        <dt>강의 정보</dt>
+        <dd>1</dd>
+        <dd>2</dd>
+        <dd>3</dd>
+        <dd>4</dd>
+        <dd>5</dd>
+        <dd>6</dd>
+      </dl>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-12 sales_chart">
+      <dl>
+        <dt>종합 데이터</dt>
+        <dd>라인 차트</dd>
+      </dl>
+    </div>
+  </div>
+</div>
+
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/qc/admin/inc/footer.php');
 ?>
