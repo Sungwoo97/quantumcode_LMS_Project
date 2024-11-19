63046b5f1e5744a95aae1236eb7c1e21a2a64a44
Naseungil <sktmddlf505@naver.com>
2024-11-19 10:15:37 +0900

    sql board 업데이트

--- quantumcode.sql
+++ quantumcode.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 24-11-18 10:25
+-- 생성 시간: 24-11-19 02:14
 -- 서버 버전: 10.4.32-MariaDB
 -- PHP 버전: 8.2.12
 
@@ -44,7 +44,7 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`idx`, `userid`, `email`, `username`, `passwd`, `regdate`, `level`, `last_login`, `end_login_date`) VALUES
-(4, 'admin', 'admin@shop.com', '관리자', '33275a8aa48ea918bd53a9181aa975f15ab0d0645398f5918a006d08675c1cb27d5c645dbd084eee56e675e25ba4019f2ecea37ca9e2995b49fcb12c096a032e', '2023-01-01 17:12:32', 100, '2024-11-18 14:51:59', NULL);
+(4, 'admin', 'admin@shop.com', '관리자', '33275a8aa48ea918bd53a9181aa975f15ab0d0645398f5918a006d08675c1cb27d5c645dbd084eee56e675e25ba4019f2ecea37ca9e2995b49fcb12c096a032e', '2023-01-01 17:12:32', 100, '2024-11-19 10:02:30', NULL);
 
 -- --------------------------------------------------------
 
@@ -113,69 +113,9 @@ INSERT INTO `board` (`pid`, `user_id`, `title`, `content`, `name`, `pw`, `date`,
 (93, NULL, '22', '22', NULL, NULL, '2024-11-18 06:05:18', NULL, 1, 0, 'free', './upload/', 0, NULL, NULL),
 (94, NULL, 'ㅅㅁㄼ11243', 'ㅈㅂㄷㅂ11253', NULL, NULL, '2024-11-18 06:12:20', NULL, 1, 0, 'qna', './upload/qqq2.png', 1, NULL, NULL),
 (95, NULL, '123456789123456789', 'ㅁㄹㄴㅁㄻㄹㄴㅁ', NULL, NULL, '2024-11-18 07:21:18', NULL, 1, 0, 'free', './upload/', 0, NULL, NULL),
-(96, NULL, '123456789ㅂㅈㄷㅂ', 'ㅂㅈㅈㅂ', NULL, NULL, '2024-11-18 07:22:23', NULL, 1, 0, 'notice', './upload/', 0, NULL, NULL),
-(97, NULL, '날짜 테스트', '날짜 테스트', NULL, NULL, '2024-11-18 07:37:46', NULL, 1, 0, 'event', './upload/', 0, '2024-11-18 00:00:00', '2024-11-19 00:00:00'),
+(96, NULL, '123456789ㅂㅈㄷㅂ', 'ㅂㅈㅈㅂ', NULL, NULL, '2024-11-18 07:22:23', NULL, 2, 0, 'notice', './upload/', 0, NULL, NULL),
 (98, NULL, '135', '135151351', NULL, NULL, '2024-11-18 09:01:25', NULL, 1, 0, 'event', './upload/', 0, '2024-11-18 00:00:00', '2024-11-27 00:00:00'),
-(100, NULL, '0000', '0000', NULL, NULL, '2024-11-18 09:09:40', NULL, 1, 0, 'event', './upload/', 0, '2024-11-18 00:00:00', '2024-11-19 00:00:00');
-
--- --------------------------------------------------------
-
---
--- 테이블 구조 `board_event`
---
-
-CREATE TABLE `board_event` (
-  `eb_pid` int(11) NOT NULL,
-  `eb_title` varchar(255) NOT NULL,
-  `eb_content` text NOT NULL,
-  `eb_event_date` date DEFAULT NULL,
-  `eb_user_id` varchar(10) DEFAULT NULL,
-  `eb_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `eb_updated_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `eb_like` int(11) DEFAULT NULL,
-  `eb_hit` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- 테이블 구조 `board_free`
---
-
-CREATE TABLE `board_free` (
-  `fb_pid` int(11) NOT NULL,
-  `fb_title` varchar(255) NOT NULL,
-  `fb_content` text NOT NULL,
-  `fb_user_id` varchar(10) DEFAULT NULL,
-  `fb_pw` varchar(50) NOT NULL,
-  `fb_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `fb_updated_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `fb_hit` int(11) DEFAULT NULL,
-  `fb_like` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- 테이블의 덤프 데이터 `board_free`
---
-
-INSERT INTO `board_free` (`fb_pid`, `fb_title`, `fb_content`, `fb_user_id`, `fb_pw`, `fb_date`, `fb_updated_date`, `fb_hit`, `fb_like`) VALUES
-(1, 'test', 'test', NULL, '', '2024-11-11 07:48:18', '2024-11-11 07:48:33', 1, NULL),
-(2, '1234', '1234', NULL, '', '2024-11-11 07:54:49', '2024-11-11 08:28:19', 24, 9);
-
--- --------------------------------------------------------
-
---
--- 테이블 구조 `board_images`
---
-
-CREATE TABLE `board_images` (
-  `imgid` int(11) NOT NULL,
-  `pid` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `filename` varchar(255) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+(101, NULL, '11', '11', NULL, NULL, '2024-11-19 01:08:32', NULL, 1, 0, 'notice', './upload/qqq3.png', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -188,54 +128,6 @@ CREATE TABLE `board_like` (
   `name` int(11) NOT NULL,
   `likes` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- 테이블 구조 `board_notice`
---
-
-CREATE TABLE `board_notice` (
-  `nb_pid` int(11) NOT NULL,
-  `nb_title` varchar(255) NOT NULL,
-  `nb_content` text NOT NULL,
-  `nb_user_id` varchar(10) DEFAULT NULL,
-  `nb_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `nb_updated_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `nb_like` int(11) DEFAULT NULL,
-  `nb_hit` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- 테이블의 덤프 데이터 `board_notice`
---
-
-INSERT INTO `board_notice` (`nb_pid`, `nb_title`, `nb_content`, `nb_user_id`, `nb_date`, `nb_updated_date`, `nb_like`, `nb_hit`) VALUES
-(1, 'test11', 'test11', NULL, '2024-11-11 07:55:07', '2024-11-11 07:56:00', 11, 13);
-
--- --------------------------------------------------------
-
---
--- 테이블 구조 `board_qna`
---
-
-CREATE TABLE `board_qna` (
-  `qb_pid` int(11) NOT NULL,
-  `qb_title` varchar(255) NOT NULL,
-  `qb_content` text NOT NULL,
-  `qb_user_id` varchar(10) DEFAULT NULL,
-  `qb_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `qb_updated_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `qb_like` int(11) DEFAULT NULL,
-  `qb_hit` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- 테이블의 덤프 데이터 `board_qna`
---
-
-INSERT INTO `board_qna` (`qb_pid`, `qb_title`, `qb_content`, `qb_user_id`, `qb_date`, `qb_updated_date`, `qb_like`, `qb_hit`) VALUES
-(1, 'test', 'test', NULL, '2024-11-11 07:48:54', '2024-11-11 07:54:16', NULL, 2);
 
 -- --------------------------------------------------------
 
@@ -386,40 +278,10 @@ ALTER TABLE `board`
   ADD PRIMARY KEY (`pid`);
 
 --
--- 테이블의 인덱스 `board_event`
---
-ALTER TABLE `board_event`
-  ADD PRIMARY KEY (`eb_pid`);
-
---
--- 테이블의 인덱스 `board_free`
---
-ALTER TABLE `board_free`
-  ADD PRIMARY KEY (`fb_pid`);
-
---
--- 테이블의 인덱스 `board_images`
---
-ALTER TABLE `board_images`
-  ADD PRIMARY KEY (`imgid`);
-
---
 -- 테이블의 인덱스 `board_like`
 --
 ALTER TABLE `board_like`
   ADD PRIMARY KEY (`pid`);
-
---
--- 테이블의 인덱스 `board_notice`
---
-ALTER TABLE `board_notice`
-  ADD PRIMARY KEY (`nb_pid`);
-
---
--- 테이블의 인덱스 `board_qna`
---
-ALTER TABLE `board_qna`
-  ADD PRIMARY KEY (`qb_pid`);
 
 --
 -- 테이블의 인덱스 `board_reply`
@@ -471,43 +333,13 @@ ALTER TABLE `admins`
 -- 테이블의 AUTO_INCREMENT `board`
 --
 ALTER TABLE `board`
-  MODIFY `pid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
-
---
--- 테이블의 AUTO_INCREMENT `board_event`
---
-ALTER TABLE `board_event`
-  MODIFY `eb_pid` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- 테이블의 AUTO_INCREMENT `board_free`
---
-ALTER TABLE `board_free`
-  MODIFY `fb_pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- 테이블의 AUTO_INCREMENT `board_images`
---
-ALTER TABLE `board_images`
-  MODIFY `imgid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `pid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
 
 --
 -- 테이블의 AUTO_INCREMENT `board_like`
 --
 ALTER TABLE `board_like`
   MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- 테이블의 AUTO_INCREMENT `board_notice`
---
-ALTER TABLE `board_notice`
-  MODIFY `nb_pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- 테이블의 AUTO_INCREMENT `board_qna`
---
-ALTER TABLE `board_qna`
-  MODIFY `qb_pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- 테이블의 AUTO_INCREMENT `board_reply`
