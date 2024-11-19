539ade196805ff533ebc724a02a33ce806109089
bluedawn123 <haemilyjh@naver.com>
2024-11-19 17:19:13 +0900

    강사 목록에 따른 분류

--- admin/teachers/teacher_list.php
+++ admin/teachers/teacher_list.php
@@ -49,22 +49,22 @@ $total_block = ceil($total_page/$block_ct);
 if($block_end > $total_page ) $block_end = $total_page;
 
 //목적에 맞게 목록 가져오기
-$sql = "SELECT * FROM teachers WHERE 1=1 $search_where ORDER BY tid ASC LIMIT $start_num, $list"; //teachers 테이블에서 모든 데이터를 조회
-$sql_highPriceToLow = "SELECT * FROM teachers WHERE 1=1 $search_where ORDER BY year_sales ASC"; //테이블에서 모든 데이터를 조회
-$sql_lowPriceToHigh = "SELECT * FROM teachers WHERE 1=1 $search_where ORDER BY year_sales DESC"; //테이블에서 모든 데이터를 조회
-$sql_highLectureToLow = "SELECT * FROM teachers WHERE 1=1 $search_where ORDER BY year_sales ASC"; //테이블에서 모든 데이터를 조회
-$sql_lowLectureToHigh = "SELECT * FROM teachers WHERE 1=1 $search_where ORDER BY year_sales DESC"; //테이블에서 모든 데이터를 조회
+
+
+
+$ordertype = $_GET['orderby'] ?? 'ASC';
+
+if(isset($_GET['orderby'])){
+  $orderColumn = 'year_sales' ;
+}else{
+  $orderColumn = 'tid' ;
+}
+
+$sql = "SELECT * FROM teachers WHERE 1=1 $search_where ORDER BY $orderColumn $ordertype LIMIT $start_num, $list"; //teachers 테이블에서 모든 데이터를 조회
 
 $result = $mysqli->query($sql); //쿼리 실행 결과
 while($data = $result->fetch_object()){
   $dataArr[] = $data;
-}
-
-function chooseSQL($sql){
-  $result = $mysqli->query($csql); 
-  while($data = $result->fetch_object()){
-  $dataArr[] = $data;
-  }
 }
 
 
@@ -77,10 +77,10 @@ function chooseSQL($sql){
     <tr>
         <td colspan="3">
           <div class="d-flex gap-3">
-            <select class="form-select mt-3" name="sort_order" >
+            <select class="form-select mt-3" id="sort_order" >
               <option value="base" selected>정렬 기준을 선택해 주세요</option>
-              <option value="highPriceToLow">매출 많은 순</option>
-              <option value="lowPriceToHigh">매출 적은 순</option>
+              <option value="desc">매출 많은 순</option>
+              <option value="asc">매출 적은 순</option>
               <option value="highLectureToLow">강의 많은 순</option>
               <option value="lowLectureToHigh">강의 적은 순</option>
             </select>
@@ -163,25 +163,15 @@ function chooseSQL($sql){
 
     sortSelect.addEventListener('change', function () {
         const selectedValue = this.value;
-
-        switch (selectedValue) {
-            case 'highPriceToLow':
-                console.log('매출 많은 순을 선택했습니다.');
-                break;
-            case 'lowPriceToHigh':
-                console.log('매출 적은 순을 선택했습니다.');
-                break;
-            case 'highLectureToLow':
-                console.log('강의 많은 순을 선택했습니다.');
-                break;
-            case 'lowLectureToHigh':
-                console.log('강의 적은 순을 선택했습니다.');
-                break;
-            default:
-                console.log('정렬 기준을 선택해 주세요.');
-        }
+        location.href= `?orderby=${selectedValue}`;
     });
 </script>
+
+
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'] . '/qc/admin/inc/footer.php');
+?>
+
 
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/qc/admin/inc/footer.php');
