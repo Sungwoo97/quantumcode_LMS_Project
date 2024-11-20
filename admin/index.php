e43e8afe01fab18ff83265ceba1ad6969b1fce37
yujin-1092 <kunche1092@gmail.com>
2024-11-20 10:59:46 +0900

    페이지네이션 축소

--- admin/index.php
+++ admin/index.php
@@ -154,11 +154,13 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/qc/admin/inc/header.php');
           </table>
            
           <nav aria-label="Page navigation example">
-            <ul class="pagination">
+            <ul class="pagination pagination-sm">
               <li class="page-item"><a class="page-link" href="#"><img src="img/icon-img/CaretLeft.svg" alt=""></a></li>
               <li class="page-item active"><a class="page-link" href="#">1</a></li>
               <li class="page-item"><a class="page-link" href="#">2</a></li>
               <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item"><a class="page-link" href="#">4</a></li>
+              <li class="page-item"><a class="page-link" href="#">5</a></li>
               <li class="page-item"><a class="page-link" href="#"><img src="img/icon-img/CaretRight.svg" alt=""></a></li>
             </ul>
           </nav>
