4c9a71e015fa46a632973c5c5eacdac9e04cff88
bluedawn123 <haemilyjh@naver.com>
2024-11-22 16:39:26 +0900

    강사 개인 페이지 만드는중

--- admin/teachers/inc/footer.php
+++ admin/teachers/inc/footer.php
@@ -1,4 +1,4 @@
-    </div>
+</div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
@@ -7,22 +7,22 @@
 
     <script>
     // 대시보드 버튼 클릭 시 index.php로 이동
-    document.addEventListener("DOMContentLoaded", ()=>{
-    const dashboardButton = document.getElementById("dashboardButton");
+    // document.addEventListener("DOMContentLoaded", ()=>{
+    // const dashboardButton = document.getElementById("dashboardButton");
 
-    dashboardButton.addEventListener("click", (e)=>{
-      e.preventDefault();
-      dashboardButton.classList.remove("collapsed");
+    // dashboardButton.addEventListener("click", (e)=>{
+    //   e.preventDefault();
+    //   dashboardButton.classList.remove("collapsed");
 
-        setTimeout(() => {
-            window.location.href = "http://<?= $_SERVER['HTTP_HOST']; ?>/qc/admin/index.php";
-        }, 500);
-        });
-    });
+    //     setTimeout(() => {
+    //         window.location.href = "http://<?= $_SERVER['HTTP_HOST']; ?>/qc/admin/teachers/index.php";
+    //     }, 500);
+    //     });
+    // });
     </script>
-  </body>
+    </body>
 
-</html>
+    </html>
     <?php
     $mysqli->close();
     ?>
