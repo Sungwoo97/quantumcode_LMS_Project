4af14d858dbf49fb9e2c7f7501ce3dcd2bcf7a04
Naseungil <sktmddlf505@naver.com>
2024-11-22 17:48:36 +0900

    글등록 취소버튼 클릭시 경로 변경

--- admin/board/board_write.php
+++ admin/board/board_write.php
@@ -60,9 +60,9 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/qc/admin/inc/header.php');
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/locales/bootstrap-datepicker.kr.min.js"></script>
 
 <script>
-  // 취소 버튼 클릭 시 이전 페이지로 돌아가기
+  // 취소 버튼 클릭 시 게시물 페이지로 돌아가기
   document.getElementById('cancle').addEventListener('click', function() {
-    window.history.back();  // 이전 페이지로 돌아가기
+    window.location.href = 'board_list.php'; 
   });
 
 
