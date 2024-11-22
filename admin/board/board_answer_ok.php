2bb1cbaf90656128d2412ad2571c9aed0dacba83
Naseungil <sktmddlf505@naver.com>
2024-11-22 13:16:32 +0900

    답변완료후 페이지이동 추가

--- admin/board/board_answer_ok.php
+++ admin/board/board_answer_ok.php
@@ -4,16 +4,28 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/qc/admin/inc/dbcon.php');
 $pid = $_GET['pid'];
 $category = $_GET['category'];
 
-echo $pid;
 
 $sql = "SELECT pid,status FROM board WHERE pid=$pid";
 $result = $mysqli->query($sql);
 $data = $result->fetch_object();
 
-echo $data -> status;
 
-if($data->status == 0 ){
+if((int)$data->status === 0 ){
   $update_sql = "UPDATE board SET status = status +1 WHERE pid=$pid";
   $update_result = $mysqli->query($update_sql);
+  if($update_result){
+    echo "<script>
+      alert('답변완료');
+      location.href='/qc/admin/board/read.php?pid='+$pid+'&category=qna';
+      </script>";
+  }else{
+    echo "<script>
+    alert('답변실패');
+     history.back();
+    </script>";
+  }
 };
 
+
+
+?>
