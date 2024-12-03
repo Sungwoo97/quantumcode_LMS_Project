9d05f45a37647195302299adc8aacaa80fb036bd
Sungwoo97 <gwaja97@gmail.com>
2024-12-04 00:03:04 +0900

    중복처리 추가

--- lecture/lecture_cart.php
+++ lecture/lecture_cart.php
@@ -13,6 +13,7 @@ if (!isset($userid)) {
   ";
 }*/
 $lid = $_GET['lid'];
+$userid = 5;
 
 $sql = "SELECT * FROM lecture_list WHERE lid = $lid";
 $result = $mysqli->query($sql);
@@ -25,15 +26,29 @@ if ($row = $result->fetch_object()) {
 }
 
 echo $tuition;
+$pid_sql = "SELECT COUNT(*) AS cnt FROM lecture_cart 
+WHERE lid=$lid AND mid = $userid ";
+$pid_result = $mysqli->query($pid_sql);
+if($pid_result){
+  $data = $pid_result->fetch_object();
+  if($data->cnt > 0){
+    echo "<script>
+    alert('중복 강의입니다.');
+    location.href = history.back();
+    </script>";
+
+  } else{
 
 // 임시로 mid는 홍길동(5) 입력
-$cart_sql = "INSERT INTO lecture_cart (mid, lid, price) VALUES ( 5, $lid, $tuition )";
+$cart_sql = "INSERT INTO lecture_cart (mid, lid, price) VALUES ( $userid, $lid, $tuition )";
 $cart_result = $mysqli->query($cart_sql);
-
-
 if ($cart_result) {
   echo "<script>
-    alert('강의가 등록되었습니다.');
+    alert('수강바구니에 등록되었습니다.');
     location.href = 'lecture_order.php';
     </script>";
 }
+  }
+}
+
+
